5d7e0f16f770d2d37e9b12cc2e8f4f78c2962790
John Hernandez <johnhdz.160@gmail.com>
2022-11-18 14:03:53 -0500

    front: navbar responsive

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,10 +4,10 @@ import Logo from "./Logo";
 const Navbar = () => {
   return (
     <header className="sticky top-0 shadow-sm font-sans z-40">
-      <nav className="py-4 bg-zinc-900 flex justify-between items-center px-36">
+      <nav className="py-4 bg-zinc-900 flex justify-between items-center px-6">
         <Logo color="text-yellow-200" />
         <div className="text-slate-200">
-          <ul className="flex space-x-8 text-normal text-slate-300 ">
+          <ul className="flex space-x-3 text-normal text-slate-300 ">
             <li>
               <Link href="/#about" scroll={false}>
                 <a className="hover:text-yellow-200">About</a>
